5c602ee80220145c6e22ef0812cf8a575148b8b3
kody.low <kodylow7@gmail.com>
2023-11-20 22:55:36 -0800

    burn: tensor slice assings

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,14 @@ use std::{collections::HashMap, fs};
 use burn::autodiff::ADBackendDecorator;
 use burn::backend::{wgpu::AutoGraphicsApi, WgpuBackend};
 use burn::tensor::{Int, Tensor};
-use plotters::backend::BitMapBackend;
-use plotters::chart::ChartBuilder;
-use plotters::drawing::IntoDrawingArea;
-use plotters::element::Rectangle;
-use plotters::style::Color;
-use plotters::style::IntoFont;
-use plotters::style::{RGBColor, WHITE};
 
-const START: char = '<';
-const END: char = '>';
+const SPECIAL: char = '.';
 
 const UNIQUE_CHAR_COUNT: usize = 28;
 
 fn main() {
     type MyBackend = WgpuBackend<AutoGraphicsApi, f32, i32>;
     type MyAutodiffBackend = ADBackendDecorator<MyBackend>;
-    let n: Tensor<MyAutodiffBackend, 2, Int> =
-        Tensor::zeros([UNIQUE_CHAR_COUNT, UNIQUE_CHAR_COUNT]);
 
     let contents =
         fs::read_to_string("./makemore/names.txt").expect("Something went wrong reading the file");
@@ -44,7 +34,7 @@ fn main() {
     let mut bigrams: Vec<String> = Vec::new();
     for name in &names {
         // hallucinate a start character '<S>' and end character '<E>'
-        let name = format!("{}{}{}", START, name, END);
+        let name = format!("{}{}{}", SPECIAL, name, SPECIAL);
         let mut chars = name.chars();
         let mut prev = chars.next().unwrap();
 
@@ -55,60 +45,23 @@ fn main() {
         }
     }
 
-    // print a heatmap of bigram counts using plotters
-    // Count bigrams
-    let mut bigram_counts: HashMap<String, usize> = HashMap::new();
-    for bigram in bigrams {
-        *bigram_counts.entry(bigram).or_insert(0) += 1;
+    // create 2 dimensional tensor of bigram counts
+    let mut bigram_counts: Tensor<MyAutodiffBackend, 2, Int> =
+        Tensor::zeros([UNIQUE_CHAR_COUNT, UNIQUE_CHAR_COUNT]);
+    for bigram in &bigrams {
+        let i = stoi[&bigram.chars().nth(0).unwrap()];
+        let j = stoi[&bigram.chars().nth(1).unwrap()];
+        let old_value = bigram_counts.clone().slice([i..i + 1, j..j + 1]);
+        let new_value = old_value + 1;
+        bigram_counts = bigram_counts.slice_assign([i..i + 1, j..j + 1], new_value);
     }
-
-    // Normalize counts to range [0, 1]
-    let max_count = *bigram_counts.values().max().unwrap() as f32;
-    let normalized_counts: HashMap<String, f32> = bigram_counts
-        .iter()
-        .map(|(bigram, count)| (bigram.clone(), *count as f32 / max_count))
-        .collect();
-
-    // Create heatmap
-    let root = BitMapBackend::new("heatmap.png", (1024, 768)).into_drawing_area();
-    root.fill(&WHITE).unwrap();
-
-    let mut chart = ChartBuilder::on(&root)
-        .caption("Heatmap of Bigram Counts", ("Arial", 50).into_font())
-        .margin(5)
-        .set_all_label_area_size(50)
-        .build_cartesian_2d(0..UNIQUE_CHAR_COUNT, 0..UNIQUE_CHAR_COUNT)
-        .unwrap();
-
-    chart
-        .configure_mesh()
-        .x_labels(UNIQUE_CHAR_COUNT)
-        .y_labels(UNIQUE_CHAR_COUNT)
-        .draw()
-        .unwrap();
-
-    for (bigram, count) in normalized_counts {
-        let x = stoi[&bigram.chars().nth(0).unwrap()];
-        let y = stoi[&bigram.chars().nth(1).unwrap()];
-        let color = ((1.0 - count) * 255.0) as u8;
-        chart
-            .draw_series(std::iter::once(Rectangle::new(
-                [(x, y), (x + 1, y + 1)],
-                RGBColor(color, color, color).filled(),
-            )))
-            .unwrap();
-    }
-
-    // Save heatmap
-    root.present().unwrap();
 }
 
 fn get_chars(names: &Vec<&str>) -> Vec<char> {
     let mut set_of_chars: HashSet<char> = names.iter().flat_map(|name| name.chars()).collect();
 
     // add start and end characters
-    set_of_chars.insert(START);
-    set_of_chars.insert(END);
+    set_of_chars.insert(SPECIAL);
 
     // convert to vector and sort
     let mut set_of_chars: Vec<char> = set_of_chars.into_iter().collect();
